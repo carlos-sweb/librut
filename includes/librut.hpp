08a158ad2840306f89e5062ff81d4308d53b05bf
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-06-19 20:30:46 -0400

    debugging the code

--- includes/librut.hpp
+++ includes/librut.hpp
@@ -7,17 +7,21 @@ using namespace absl;
 namespace ppRut{
 	class rut{
 		private:
+			string rutRaw="";
 			string body="";
 			string digitS="";
 			int digitN=-1;
 			int total = 0;
 			int calculate();
-			string numberToDigitV();
+			string numberToDigitV() const;
+			void parser();
 		public:	
-		rut(string rutToParse);
-		bool checkDigit(string localDigit);
-		bool checkDigit();
-		string format(string separate_miles=".",string separate_digit="-");
-		string getDigit();
+			rut(const string&,const string&);
+			rut(const string&);
+			rut(const int&);
+			bool checkDigit(const string&) const ;
+			bool checkDigit() const;
+			string format(const string &separate_miles=".",const string &separate_digit="-");
+			string getDigit();
 	};
 }
--- src/librut.cpp
+++ src/librut.cpp
@@ -1,52 +1,57 @@
 #include <string>
 #include <librut.hpp>
+#include <utility>
 using namespace std;
 using namespace absl;
 
 namespace ppRut{
-	rut::rut(string rutToParse){
-		if(!rutToParse.empty()){
-				vector<string> v = StrSplit(rutToParse, "-");			
-				string bodyDirty = v.size() >= 1 ? v.at(0) : "";				
-				for(char c : bodyDirty ){if(isdigit(static_cast<unsigned char>(c))){body.push_back(c);}}
-				digitS = v.size() >= 2 ? v.at(1): "";				
-				string TmpBody = body;
-				reverse(TmpBody.begin(),TmpBody.end());
-				int multiplicador=2;
-				for(char c : TmpBody){
-					if(isdigit(static_cast<unsigned char>(c))){
-						int cInit = ( c-'0') * multiplicador;												
-						total = (total+cInit);
-						if(multiplicador==7){multiplicador=2;}else{multiplicador++;}
-					}
-				}
-			}		
-			calculate();		
-	}
+	rut::rut(const string &rutToParse,const string &digitS):rutRaw(std::move(rutToParse)),digitS(digitS){parser();}
+	rut::rut(const int &rutToParse):rutRaw(to_string(rutToParse)){parser();}
+	rut::rut(const string &rutToParse):rutRaw(rutToParse){parser();}
 
-	bool rut::checkDigit(string localDigit){return (numberToDigitV() == localDigit);}
-	bool rut::checkDigit(){return (numberToDigitV() == digitS);}
+	void rut::parser(){
+		if(!rutRaw.empty()){
+			vector<string> v = StrSplit(rutRaw, "-");
+			string bodyDirty = v.size() >= 1 ? v.at(0) : "";
+			for(char c : bodyDirty ){if(isdigit(static_cast<unsigned char>(c))){body.push_back(c);}}			 
+			if(v.size() >= 2){
+				digitS	 = v.at(1);
+			}
+			string TmpBody = body;
+			reverse(TmpBody.begin(),TmpBody.end());
+			int multiplicador=2;
+			for(char c : TmpBody){
+				if(isdigit(static_cast<unsigned char>(c))){
+					int cInit = ( c-'0') * multiplicador;
+					total = (total+cInit);
+					if(multiplicador==7){multiplicador=2;}else{multiplicador++;}
+				}
+			}
+		}
+		calculate();
+	}
+	bool rut::checkDigit(const string &localDigit) const {return (numberToDigitV() == localDigit);}
+	bool rut::checkDigit() const {return (numberToDigitV() == digitS);}
 	int rut::calculate(){
-		digitN = 11-(total-((total/11)*11)); 		
+		digitN = 11-(total-((total/11)*11));
 		return digitN;
 	}
-	string rut::numberToDigitV(){
-		return digitN == 11 ? "0" : ( digitN == 10 ? "k" : to_string(digitN) );		
+	string rut::numberToDigitV() const {
+		return digitN == 11 ? "0" : ( digitN == 10 ? "k" : to_string(digitN) );
 	}
-	string rut::format(string separate_miles,string separate_digit){
-			string output = "";
-			string TmpBody = "";
-			int position_separate=0;
-			for(int i = (body.size()-1);i>=0;i-- ){
-				if(position_separate == 3){TmpBody.append(separate_miles);position_separate = 1;}else{position_separate++;}
-				TmpBody.push_back(body.at(i));
-			}
-			reverse(TmpBody.begin(),TmpBody.end());						
-			output.append(TmpBody);
-			output.append(separate_digit);
-			output.append(numberToDigitV());
-			return output;
+	string rut::format(const string &separate_miles,const string &separate_digit){
+		string output = "",
+		TmpBody = "";
+		int position_separate=0;
+		for(int i = (body.size()-1);i>=0;i-- ){
+			if(position_separate == 3){TmpBody.append(separate_miles);position_separate = 1;}else{position_separate++;}
+			TmpBody.push_back(body.at(i));
+		}
+		reverse(TmpBody.begin(),TmpBody.end());
+		output.append(TmpBody);
+		output.append(separate_digit);
+		output.append(numberToDigitV());
+		return output;
 	}
-
-	string rut::getDigit(){ return numberToDigitV();}	
+	string rut::getDigit(){ return numberToDigitV();}
 }
